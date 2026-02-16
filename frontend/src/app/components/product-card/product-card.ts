8df1613217e93215cccec09cd3e00bb525589be2
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-16 21:51:30 +0200

    feat(home/ui): finished product-card component

---
+++ frontend/src/app/components/product-card/product-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.html',
+  styles: ``,
+})
+export class ProductCard {
+
+}
