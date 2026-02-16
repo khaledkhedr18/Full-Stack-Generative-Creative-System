5470a758b7049aaee3d7968b0a56a88f51ed743f
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-16 23:17:02 +0200

    feat(home/ui): finished home page

---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Footer } from "../../components/footer/footer";
+import { Header } from "../../components/header/header";
+import { RouterLink } from "@angular/router";
+import { ProductCard } from "../../components/product-card/product-card";
+
+@Component({
+  selector: 'app-home',
+  imports: [Footer, Header, RouterLink, ProductCard],
+  templateUrl: './home.html',
+  styles: ``,
+})
+export class Home {
+
+}
