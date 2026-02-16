b2cc8f98084294c5d3ff7904805a81249e3fa98b
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-16 23:15:03 +0200

    chore(header/ui): add login-sign up buttons

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,5 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styles: ``,
 })
 export class Header {
-
+  isLoggedIn = false;
 }
