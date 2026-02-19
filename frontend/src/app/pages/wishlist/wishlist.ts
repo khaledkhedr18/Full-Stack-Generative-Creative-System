28be02358b053f1dd84a966335743109fc57a2dc
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-19 04:32:07 +0200

    feat(wishlist/ui): finished wishlist page ui

---
+++ frontend/src/app/pages/wishlist/wishlist.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { WishlistCard } from '../../components/wishlist-card/wishlist-card';
+
+@Component({
+  selector: 'app-wishlist',
+  imports: [WishlistCard],
+  templateUrl: './wishlist.html',
+  styles: ``,
+})
+export class Wishlist {}
