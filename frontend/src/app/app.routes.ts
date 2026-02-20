bb4d2a7ec1de92a05a7690ecd79dc0311812273f
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-20 06:01:13 +0200

    chore: update project routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Login } from './pages/login/login';
 import { SignUp } from './pages/sign-up/sign-up';
 import { Home } from './pages/home/home';
 import { Products } from './pages/products/products';
+import { Wishlist } from './pages/wishlist/wishlist';
+import { Cart } from './pages/cart/cart';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'login', component: Login, data: { hideHeaderFooter: true } },
   { path: 'signup', component: SignUp, data: { hideHeaderFooter: true } },
-  { path: 'products', component: Products },
+  { path: 'shop', component: Products },
+  { path: 'wishlist', component: Wishlist },
+  { path: 'cart', component: Cart },
 ];
