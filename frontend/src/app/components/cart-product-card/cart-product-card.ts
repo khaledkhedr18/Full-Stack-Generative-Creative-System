4a9c500d51d608c4c64a3d9f3fcc2d900c43b87b
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-20 03:24:52 +0200

    feat(cart/ui): finished cart-product card ui

---
+++ frontend/src/app/components/cart-product-card/cart-product-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-product-card',
+  imports: [],
+  templateUrl: './cart-product-card.html',
+  styles: ``,
+})
+export class CartProductCard {
+
+}
