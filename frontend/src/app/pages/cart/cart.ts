7cc7d46c31ec0e3ab8cc4a4ebb5e9b47b5176bef
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-20 06:29:56 +0200

    fix(cart): correct routerLink for proceed checkout button

--- frontend/src/app/pages/cart/cart.ts
+++ frontend/src/app/pages/cart/cart.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CartProductCard } from '../../components/cart-product-card/cart-product-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartProductCard],
+  imports: [CartProductCard, RouterLink],
   templateUrl: './cart.html',
   styles: ``,
 })
