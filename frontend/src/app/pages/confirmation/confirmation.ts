b219fd20ac1cbc3ae152fa3df6a10ab110af7813
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-21 08:30:39 +0200

    feat(confirmation/ui): finished confirmation page ui

---
+++ frontend/src/app/pages/confirmation/confirmation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CheckoutProductCard } from '../../components/checkout-product-card/checkout-product-card';
+
+@Component({
+  selector: 'app-confirmation',
+  imports: [CheckoutProductCard],
+  templateUrl: './confirmation.html',
+  styles: ``,
+})
+export class Confirmation {}
