b50f97a1318eaf4d0a483d1500809033001d4a70
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-21 06:56:55 +0200

    feat(checkout/ui): added checkout product card

---
+++ frontend/src/app/components/checkout-product-card/checkout-product-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-product-card',
+  imports: [],
+  templateUrl: './checkout-product-card.html',
+  styles: ``,
+})
+export class CheckoutProductCard {}
