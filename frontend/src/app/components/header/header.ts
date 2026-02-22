bd4b3729209e8db80b222d37eb23699d61ad329c
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-22 22:56:17 +0200

    refactor(icons): replace inline SVGs with ng-icon components

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideHeart, lucideLogOut, lucideMenu, lucideShoppingCart } from '@ng-icons/lucide';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIcon],
+  providers: [provideIcons({ lucideHeart, lucideShoppingCart, lucideMenu, lucideLogOut })],
   templateUrl: './header.html',
   styles: ``,
 })
