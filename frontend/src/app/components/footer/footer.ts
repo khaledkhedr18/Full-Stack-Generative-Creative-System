84efbc9569edc0420d695713b3d65a92228a6e66
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-22 23:45:02 +0200

    refactor(footer/icons): replace inline SVGs with ng-icon components

--- frontend/src/app/components/footer/footer.ts
+++ frontend/src/app/components/footer/footer.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideFacebook, lucideInstagram, lucideMoveRight, lucideTwitter } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIcon],
+  providers: [provideIcons({ lucideFacebook, lucideInstagram, lucideTwitter, lucideMoveRight })],
   templateUrl: './footer.html',
   styles: ``,
 })
