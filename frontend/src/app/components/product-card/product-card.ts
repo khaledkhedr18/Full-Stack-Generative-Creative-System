12e80b9ec9ddf260b52ba2667de18733e39c81a4
mohamed ashraf <mohamedtaghian12@gmail.com>
2026-02-24 00:55:46 +0200

    feat(home/logic): displaying products

--- frontend/src/app/components/product-card/product-card.ts
+++ frontend/src/app/components/product-card/product-card.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
-import { ColorFilter } from "../color-filter/color-filter";
+import { Component, Input, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product, ProductVariant } from '../../utils/product-interface';
 
 @Component({
   selector: 'app-product-card',
-  imports: [RouterLink, ColorFilter],
+  imports: [RouterLink],
   templateUrl: './product-card.html',
   styles: ``,
 })
 export class ProductCard {
+  @Input() data: Product = {} as Product;
+  selectedVariant = signal<ProductVariant | null>(null);
 
+  selectVariant(variant: ProductVariant) {
+    this.selectedVariant.set(variant);
+  }
 }
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { RouterLink } from '@angular/router';
 import { ProductCard } from '../../components/product-card/product-card';
@@ -11,6 +11,8 @@ import {
   lucideSparkles,
   lucideVan,
 } from '@ng-icons/lucide';
+import { ProductServices } from '../../services/product-services';
+import { Product } from '../../utils/product-interface';
 
 @Component({
   selector: 'app-home',
@@ -29,4 +31,32 @@ import {
   templateUrl: './home.html',
   styles: ``,
 })
-export class Home {}
+export class Home {
+  constructor(private readonly productService: ProductServices) {}
+
+  products = signal<Product[]>([]);
+  loading = signal(true);
+  error = signal<string | null>(null);
+
+  loadProducts(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.productService.getProducts(1, 4).subscribe({
+      next: (res) => {
+        console.log(res.data);
+        this.products.set(res.data);
+        this.loading.set(false);
+      },
+      error: (err) => {
+        this.error.set('Failed to load products. Please try again.');
+        this.loading.set(false);
+        console.log(err);
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+}
